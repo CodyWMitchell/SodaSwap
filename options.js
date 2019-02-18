cd88dbbb53e417d0c368b9c70715af81a9795ab2
Cody <bigbrocode@yahoo.com>
2019-02-18 14:19:04 -0500

    Attempting to fix storage in chrome

--- options.js
+++ options.js
@@ -1,8 +1,8 @@
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('namePref').value;
+  var preference = document.getElementById('namePref').value;
   chrome.storage.sync.set({
-    namePreference: color,
+    namePreference: preference,
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
